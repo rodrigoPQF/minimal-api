ce87042f723a9a10b8781ea7da87a782a469f393
rodrigopr404 <rodrigopr404@gmail.com>
2024-09-30 17:20:05 -0300

    ✨ chore - Configuração inicial e implementação da primeira rota

--- Program.cs
+++ Program.cs
@@ -1,13 +1,30 @@
-using MinimalApi.Domain.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Domain.DTOs;
+using minimal_api.Domain.Services;
+using minimal_api.Infra.Database;
+using minimal_api.Infra.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAdminService, AdminService>();
+
+builder.Services.AddDbContext<DatabaseContext>(
+  options =>
+  {
+    options.UseNpgsql(builder.Configuration.GetConnectionString("postgres"));
+  }
+);
+
 var app = builder.Build();
+
+
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (LoginDTO logintDTO) =>
+app.MapPost("/login", ([FromBody] LoginDTO logintDTO, IAdminService adminService) =>
 {
-  if (logintDTO.Email == "adm@test.com" && logintDTO.Senha == "123456")
+  if (adminService.Login(logintDTO) != null)
     return Results.Ok("Login efetuado com sucesso");
   else
     return Results.Unauthorized();
