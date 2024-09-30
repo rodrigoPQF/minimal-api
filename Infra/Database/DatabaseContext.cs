ccb6d1d14d6d5bac0784347f39fd3cee23ca5c73
rodrigopr404 <rodrigopr404@gmail.com>
2024-09-30 17:17:22 -0300

    🗃️ chore(database) - Configurando a conexão do banco de dados

--- Infra/Database/DatabaseContext.cs
+++ Infra/Database/DatabaseContext.cs
@@ -2,15 +2,43 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Domain.Entities;
 
-namespace MinimalApi.Infra.Database
+namespace minimal_api.Infra.Database
 {
-    public class DatabaseContext
+    public class DatabaseContext : DbContext
     {
 
-        public DatabaseContext()
+        private readonly IConfiguration _configuration;
+        public DatabaseContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public DbSet<Admin> Admins { get; set; } = default!;
+        public DbSet<Veiculo> Veiculos { get; set; } = default!;
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Admin>().HasData(
+                new Admin { Id = 1, Email = "adm@test.com", Senha = "123456", Perfil = "admin" }
+            );
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
 
+            if (!optionsBuilder.IsConfigured)
+            {
+                var stringConnection = _configuration.GetConnectionString("postgres")?.ToString();
+
+                if (!string.IsNullOrEmpty(stringConnection))
+                {
+                    optionsBuilder.UseNpgsql(stringConnection);
+
+                }
+            }
         }
     }
 }
