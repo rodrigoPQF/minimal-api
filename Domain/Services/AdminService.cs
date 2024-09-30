aa95d46711bc7e65294a1fe87af13c9bc832ad6a
rodrigopr404 <rodrigopr404@gmail.com>
2024-09-30 17:19:34 -0300

    🚧 feat(services) - Implementando os serviços

---
+++ Domain/Services/AdminService.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore.Storage;
+using minimal_api.Domain.DTOs;
+using minimal_api.Domain.Entities;
+using minimal_api.Infra.Database;
+using minimal_api.Infra.Interfaces;
+
+namespace minimal_api.Domain.Services;
+
+
+public class AdminService : IAdminService
+{
+    private readonly DatabaseContext _context;
+    public AdminService(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+
+    public Admin? Login(LoginDTO loginDTO)
+    {
+        var admin = _context.Admins.Where(a => a.Email == loginDTO.Email && a.Senha == loginDTO.Senha).FirstOrDefault();
+
+        return admin;
+    }
+}
---
+++ Domain/Services/VeiculoService.cs
@@ -0,0 +1,67 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using minimal_api.Domain.DTOs;
+using minimal_api.Domain.Entities;
+using minimal_api.Infra.Database;
+using minimal_api.Infra.Interfaces;
+
+namespace minimal_api.Domain.Services;
+
+
+public class VeiculoService : IVeiculoService
+{
+    private readonly DatabaseContext _context;
+    public VeiculoService(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public Veiculo AtualizaVeiculo(int id, Veiculo veiculo)
+    {
+        _context.Veiculos.Update(new Veiculo { Id = id, Nome = veiculo.Nome, Ano = veiculo.Ano, Marca = veiculo.Marca });
+        _context.SaveChanges();
+        return veiculo;
+    }
+
+    public Veiculo? BuscaPorId(int id)
+    {
+        return _context.Veiculos.Where(v => v.Id == id).FirstOrDefault();
+    }
+
+    public void CadastraVeiculo(Veiculo veiculo)
+    {
+        _context.Veiculos.Add(veiculo);
+        _context.SaveChanges();
+
+    }
+
+    public bool DeletaVeiculo(int id)
+    {
+        Veiculo? veiculo = _context.Veiculos.Find(id);
+        if (veiculo == null)
+        {
+            return false;
+        }
+
+        _context.Veiculos.Remove(veiculo);
+        _context.SaveChanges();
+
+        return true;
+    }
+
+
+    public List<Veiculo> TodosVeiculos(int page = 1, string? nome = null, string? marca = null)
+    {
+        var query = _context.Veiculos.AsQueryable();
+
+        if (!string.IsNullOrEmpty(nome))
+        {
+            query = query.Where(v => EF.Functions.Like(v.Nome.ToLower(), $"%{nome}%"));
+        }
+
+        int itensPorPagina = 10;
+        query = query.Skip((page - 1) * itensPorPagina).Take(itensPorPagina);
+
+        return [.. query];
+    }
+}
